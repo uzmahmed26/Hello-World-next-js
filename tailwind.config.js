63e795033c7c09e39f42610832aca8ebf0795871
Uzma Ahmed <uzmahmed26.uk@gmail.com>
2024-09-26 16:03:24 +0500

    resolve error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', // App directory ke andar sari files
+    './pages/**/*.{js,ts,jsx,tsx}', // Agar pages directory bhi ho
+    './components/**/*.{js,ts,jsx,tsx}', // Components folder bhi include ho
+    ],
   theme: {
     extend: {},
   },
